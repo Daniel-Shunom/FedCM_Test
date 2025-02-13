ae612dd78e3d86cb0bbd464381064787ed25739b
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-02-13 00:31:33 -0500

    Fixed root layout issue

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="application/index" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
