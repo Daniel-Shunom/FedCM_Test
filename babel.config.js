d4a7e7938314b1d742b66d03be7271847fb5cdbb
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-02-13 00:31:12 -0500

    removed API for depracated dotenv package

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,6 @@
 module.exports = function(api) {
     api.cache(false);
     return {
-      plugins: [
-        [
-          'module:react-native-dotenv',
-          {
-            envName: 'APP_ENV',
-            moduleName: '@env',
-            path: '.env',
-            blocklist: null,
-            allowlist: null,
-            blacklist: null, // DEPRECATED
-            whitelist: null, // DEPRECATED
-            safe: false,
-            allowUndefined: true,
-            verbose: false,
-          },
-        ],
-      ],
+        presets: ['babel-preset-expo'],
     };
-  };
+};
