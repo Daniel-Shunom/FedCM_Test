52578eb6454eebb5bf08493e57929e19d905f494
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-02-27 09:13:04 -0500

    removed unnecessary debug prints

--- protected/tokenhandlers/handler.ts
+++ protected/tokenhandlers/handler.ts
@@ -4,14 +4,14 @@ import { getCourses } from "../courses/courses";
 
 export const handleCredentialResponse = async (response: any) => {
   const navigation = router;
-  console.log("Encoded JWT ID token: " + response.credential);
+  console.log('Handle Response');
 
   try {
     const res = await fetch(`${process.env.EXPO_PUBLIC_GOOGLE_END_POINT}${response.credential}`);
     const data = await res.json();
     await fiboAuth({ credential: response.credential })
       .catch((error) => {console.error("Failed to fetch user data:", error)});
-    console.log("Data:", data);
+    console.log("Retrived user Data");
     updateAuthState(data);
     const session_token = await localStorage.getItem('fibo_session_token');
     await getCourses(session_token);
