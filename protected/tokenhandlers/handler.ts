e42b18b4153e48e912e78c1bdbc8ea43c5dd9cc4
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-03-06 16:47:15 -0500

    Extracted User details from login flow

--- protected/tokenhandlers/handler.ts
+++ protected/tokenhandlers/handler.ts
@@ -1,10 +1,17 @@
 import { router } from "expo-router";
 import { fiboAuth, updateAuthState } from "../auth/auth";
 import { getCourses } from "../courses/courses";
+import { User } from "@/context/types";
 
-export const handleCredentialResponse = async (response: any) => {
+export const handleCredentialResponse = async (response: any): Promise<User> => {
+  
   const navigation = router;
-  console.log('Handle Response');
+  const loaded_user: User = {
+    email: null,
+    name: null,
+    fiboToken: null,
+    clientToken: null
+  }
 
   try {
     const res = await fetch(`${process.env.EXPO_PUBLIC_GOOGLE_END_POINT}${response.credential}`);
@@ -17,10 +24,17 @@ export const handleCredentialResponse = async (response: any) => {
     .then(()=> console.log('Retrived UserData'));
 
     const session_token = localStorage.getItem('fibo_session_token');
+    loaded_user.email = data.email;
+    loaded_user.name = data.name;
+    loaded_user.fiboToken = session_token;
+    loaded_user.clientToken = response.credential;
+
     await getCourses(session_token);
 
     navigation.replace('/application');
   } catch (error) {
     console.error("Failed to fetch user data:", error);
   }
+
+  return loaded_user
 };
