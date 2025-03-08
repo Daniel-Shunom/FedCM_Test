0a396d24e40116c656628d7e7b827137659e00a1
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-03-08 01:41:12 -0500

    condensed component into WelcomePage

--- app/index.tsx
+++ app/index.tsx
@@ -6,39 +6,14 @@ Description: Application entry point with login screen.
 */
 
 import React from 'react';
-import { Platform, StyleSheet, Image } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { ThemedView } from '@/components/ThemedView';
-import { LoginForm } from '@/components/LoginForm';
-import { FiboButton } from '@/components/ui/Buttons/buttons';
-import LottieView from 'lottie-react-native'
+import { WelcomePage } from '@/components/ui/Pages/WelcomePage';
 
-export default function LoginScreen() {
-  const img = require('../assets/images/adaptive-icon.png')
-  const width = 250
-  const content = {
-    btn1: 'Hello',
-    btn2: 'login'
-  }
+export default function LandingScreen() {
   return (
     <ThemedView style={styles.container}>
-      {/*<LoginForm
-        //here just incase we want to use a regular login option
-        onSubmit={(email, password) => {
-        }}
-      />/**/}
-      {/*<Image source={img}/>/**/}
-      <FiboButton
-        Color='#fff'
-        BorderRadius={10}
-        Width={width}
-        Content={content.btn1}
-      />
-      <FiboButton
-        Color='#0ff'
-        BorderRadius={10}
-        Width={width}
-        Content={content.btn2} 
-      />
+      <WelcomePage />
     </ThemedView>
   );
 }
@@ -47,7 +22,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: 80,
+    justifyContent: 'center',
   },
 });
