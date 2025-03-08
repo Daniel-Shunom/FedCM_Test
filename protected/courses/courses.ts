61a4d5532045b45925c0711dbee4994260cda92e
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-03-08 17:11:29 -0500

    moodleAPI function calls

--- protected/courses/courses.ts
+++ protected/courses/courses.ts
@@ -12,3 +12,56 @@ export async function getCourses(token: string | null): Promise<any> {
   console.log(response)
   return response
 }
+
+export async function getCoreCourses(): Promise<JSON|undefined> {
+  const token = process.env.EXPO_PUBLIC_MOODLE_TOKEN;
+  const MoodleUrl: string|undefined = process.env.EXPO_PUBLIC_MOODLE_URL;
+  const method: string|undefined = process.env.EXPO_PUBLIC_MOODLE_COURSES;
+  const format: string|undefined = process.env.EXPO_PUBLIC_MOODLE_FORMAT;
+  const url: string|undefined = `${MoodleUrl}/webservice/rest/server.php`;
+  const body: string|undefined = `wstoken=${token}&wsfunction=${method}&moodlewsrestformat=${format}`;
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: body
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log('Moodle courses API response: ', result)
+    return result;
+  } catch (error) {
+    console.error("Error calling Moodle API:", error);
+    return undefined;
+  }
+}
+
+export async function getCoreCalendarEvents(): Promise<JSON|undefined> {
+  const token = process.env.EXPO_PUBLIC_MOODLE_TOKEN;
+  const MoodleUrl: string|undefined = process.env.EXPO_PUBLIC_MOODLE_URL;
+  const method: string|undefined = process.env.EXPO_PUBLIC_MOODLE_CALENDAR_EVENTS;
+  const format: string|undefined = process.env.EXPO_PUBLIC_MOODLE_FORMAT;
+  const url: string|undefined = `${MoodleUrl}/webservice/rest/server.php`;
+  const body: string|undefined = `wstoken=${token}&wsfunction=${method}&moodlewsrestformat=${format}`;
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: body
+    })
+    return response.json()
+  } catch (e) {
+    console.error('ERROR GETTING CALENDAR EVENTS: ', e)
+    return
+  }
+}
