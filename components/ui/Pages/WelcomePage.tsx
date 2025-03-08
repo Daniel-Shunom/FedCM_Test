d5aaf6ea890854fee32c8d0f40520edf699c660e
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-03-08 01:41:43 -0500

    Welcome page component

---
+++ components/ui/Pages/WelcomePage.tsx
@@ -0,0 +1,38 @@
+import { View, Image } from "react-native"
+import { FiboButton } from "../Buttons/buttons"
+import { router } from "expo-router"
+
+export function WelcomePage () {
+  const img = require('@/assets/images/adaptive-icon.png')
+  const width = 250
+  const content = {
+    btn1: 'Hello',
+    btn2: 'login'
+  }
+
+  return (
+    <View style={{justifyContent: 'center', alignItems: 'center'}}>
+      {/*<LoginForm
+        //here just incase we want to use a regular login option
+        onSubmit={(email, password) => {
+            }}
+            />/**/}
+      <View>
+        {<Image source={img} style={{width: 300, height: 300, marginBottom: 100, padding: 'auto'}}/>/**/}
+      </View>
+      <FiboButton
+        Color='#fff'
+        BorderRadius={10}
+        Width={width}
+        Content={content.btn1}
+      />
+      <FiboButton
+        Color='#0ff'
+        BorderRadius={10}
+        Width={width}
+        Content={content.btn2}
+        funcPress={()=> {router.navigate('/authentication/login')}} 
+      />
+    </View>
+  )
+}
