7a292df48ea1c35a248ef8295dfeb9e1ffbecefa
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-03-08 01:38:53 -0500

    disabled temp rerouting check and replaced screens with slot

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,7 @@ Description: AI generated boilerplate code for default layout.
 
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { router, Stack } from 'expo-router';
+import { router, Slot, Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
@@ -32,7 +32,7 @@ export default function RootLayout() {
       SplashScreen.hideAsync();
     }
 
-    if (!user) {
+    /*if (!user) {
       console.log('User not found, redirecting to login screen')
       router.replace('/');
     }/**/
@@ -50,8 +50,7 @@ export default function RootLayout() {
             headerShown: false,
           }}
         >
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
+          <Slot />
         </Stack>
         {<StatusBar style="auto" />/**/}
       </ThemeProvider>
