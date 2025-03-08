da46c567477f6fdbeeaaffc2d8bace117a351646
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-03-07 22:19:00 -0500

    used buttons

--- app/index.tsx
+++ app/index.tsx
@@ -6,17 +6,38 @@ Description: Application entry point with login screen.
 */
 
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet, Image } from 'react-native';
 import { ThemedView } from '@/components/ThemedView';
 import { LoginForm } from '@/components/LoginForm';
+import { FiboButton } from '@/components/ui/Buttons/buttons';
+import LottieView from 'lottie-react-native'
 
 export default function LoginScreen() {
+  const img = require('../assets/images/adaptive-icon.png')
+  const width = 250
+  const content = {
+    btn1: 'Hello',
+    btn2: 'login'
+  }
   return (
     <ThemedView style={styles.container}>
-      <LoginForm
+      {/*<LoginForm
         //here just incase we want to use a regular login option
         onSubmit={(email, password) => {
         }}
+      />/**/}
+      {/*<Image source={img}/>/**/}
+      <FiboButton
+        Color='#fff'
+        BorderRadius={10}
+        Width={width}
+        Content={content.btn1}
+      />
+      <FiboButton
+        Color='#0ff'
+        BorderRadius={10}
+        Width={width}
+        Content={content.btn2} 
       />
     </ThemedView>
   );
@@ -26,7 +47,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
+    justifyContent: 'flex-end',
+    padding: 80,
   },
 });
